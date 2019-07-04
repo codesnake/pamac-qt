fe4699608da9e242c1abee3cf8a8c3fa49c2fb3a
LordTermor <agrinev98@gmail.com>
2019-07-05 01:08:12 +0400

    Package objects memory management improvements

--- libqpamac/include/RepoPackage.h
+++ libqpamac/include/RepoPackage.h
@@ -5,82 +5,103 @@
 #include <QMetaType>
 #include <memory>
 #include "Utils.h"
+#include <QtDebug>
 namespace LibQPamac {
-class RepoPackage
+class RepoPackage:public PamacPackage
 {
     Q_GADGET
-private:
-    PamacPackage* m_package;
-
 public:
 
     RepoPackage()=default;
 
-    RepoPackage(PamacPackage* package):m_package(package){}
+    RepoPackage(PamacPackage* package){
+        *static_cast<PamacPackage*>(this)=*package;
+        g_object_ref(static_cast<PamacPackage*>(this));
 
-    PAMAC_QT_STRING_PROPERTY_GET(name,pamac_package_get_name(m_package))
+    }
+    RepoPackage(const RepoPackage& another){
+        *static_cast<PamacPackage*>(this) = *static_cast<const PamacPackage*>(&another);
+        g_object_ref(static_cast<PamacPackage*>(this));
+    }
+    RepoPackage operator =(const RepoPackage& another){
+        *static_cast<PamacPackage*>(this) = *static_cast<const PamacPackage*>(&another);
+        g_object_ref(static_cast<PamacPackage*>(this));
+        return this;
+    }
 
-    PAMAC_QT_STRING_PROPERTY_GET(appName,pamac_package_get_app_name(m_package))
+    PAMAC_QT_STRING_PROPERTY_GET(name,pamac_package_get_name(this))
 
-    PAMAC_QT_STRING_PROPERTY_GET(version,pamac_package_get_version(m_package))
+    PAMAC_QT_STRING_PROPERTY_GET(appName,pamac_package_get_app_name(this))
 
-    PAMAC_QT_STRING_PROPERTY_GET(installedVersion,pamac_package_get_installed_version(m_package))
+    PAMAC_QT_STRING_PROPERTY_GET(version,pamac_package_get_version(this))
 
-    PAMAC_QT_STRING_PROPERTY_GET(desc,pamac_package_get_desc(m_package))
+    PAMAC_QT_STRING_PROPERTY_GET(installedVersion,pamac_package_get_installed_version(this))
 
-    PAMAC_QT_URL_PROPERTY_GET(iconUrl,pamac_package_get_icon(m_package))
+    PAMAC_QT_STRING_PROPERTY_GET(desc,pamac_package_get_desc(this))
 
-    PAMAC_QT_STRING_PROPERTY_GET(repo,pamac_package_get_repo(m_package))
+    PAMAC_QT_URL_PROPERTY_GET(iconUrl,pamac_package_get_icon(this))
 
-    PAMAC_QT_INT_PROPERTY_GET(size,pamac_package_get_installed_size(m_package))
+    PAMAC_QT_STRING_PROPERTY_GET(repo,pamac_package_get_repo(this))
 
-    PAMAC_QT_INT_PROPERTY_GET(downloadSize,pamac_package_get_download_size(m_package))
+    PAMAC_QT_INT_PROPERTY_GET(size,pamac_package_get_installed_size(this))
+
+    PAMAC_QT_INT_PROPERTY_GET(downloadSize,pamac_package_get_download_size(this))
 
 
-    operator PamacPackage*()
-    {
-        return m_package;
+    ~RepoPackage(){
+        g_object_unref(static_cast<PamacPackage*>(this));
     }
 
 };
 
-class RepoPackageDetails
+class RepoPackageDetails:public PamacPackageDetails
 {
     Q_GADGET
 public:
 
-    RepoPackageDetails():m_details(nullptr){}
+    RepoPackageDetails()= default;
+    RepoPackageDetails(const RepoPackageDetails& another){
+        *static_cast<PamacPackageDetails*>(this) = *static_cast<const PamacPackageDetails*>(&another);
+        g_object_ref(this);
+    }
+    RepoPackageDetails operator =(const RepoPackageDetails& another){
+        *static_cast<PamacPackageDetails*>(this) = *static_cast<const PamacPackageDetails*>(&another);
+        g_object_ref(this);
+        return this;
+    }
+    RepoPackageDetails(PamacPackageDetails* details){
+        *static_cast<PamacPackageDetails*>(this)=*details;
+        g_object_ref(this);
+    }
+    ~RepoPackageDetails(){
+        g_object_unref(this);
+    }
+    PAMAC_QT_STRING_PROPERTY_GET(name,pamac_package_details_get_name(this))
+    PAMAC_QT_STRING_PROPERTY_GET(appName,pamac_package_details_get_app_name(this))
+    PAMAC_QT_STRING_PROPERTY_GET(desc,pamac_package_details_get_desc(this))
 
-    RepoPackageDetails(PamacPackageDetails* details):m_details(std::shared_ptr<PamacPackageDetails>(details,g_object_unref)){}
+    PAMAC_QT_STRING_PROPERTY_GET(longDesc,pamac_package_details_get_long_desc(this))
+    PAMAC_QT_STRING_PROPERTY_GET(repo,pamac_package_details_get_repo(this))
+    PAMAC_QT_STRING_PROPERTY_GET(packager,pamac_package_details_get_packager(this))
 
-    PAMAC_QT_STRING_PROPERTY_GET(name,pamac_package_details_get_name(m_details.get()))
-    PAMAC_QT_STRING_PROPERTY_GET(appName,pamac_package_details_get_app_name(m_details.get()))
-    PAMAC_QT_STRING_PROPERTY_GET(desc,pamac_package_details_get_desc(m_details.get()))
+    PAMAC_QT_URL_PROPERTY_GET(url,pamac_package_details_get_url(this))
 
-    PAMAC_QT_STRING_PROPERTY_GET(longDesc,pamac_package_details_get_long_desc(m_details.get()))
-    PAMAC_QT_STRING_PROPERTY_GET(repo,pamac_package_details_get_repo(m_details.get()))
-    PAMAC_QT_STRING_PROPERTY_GET(packager,pamac_package_details_get_packager(m_details.get()))
+    PAMAC_QT_URL_PROPERTY_GET(iconUrl,pamac_package_details_get_icon(this))
 
-    PAMAC_QT_URL_PROPERTY_GET(url,pamac_package_details_get_url(m_details.get()))
+    PAMAC_QT_URL_PROPERTY_GET(screenshotUrl,pamac_package_details_get_screenshot(this))
 
-    PAMAC_QT_URL_PROPERTY_GET(iconUrl,pamac_package_details_get_icon(m_details.get()))
+    PAMAC_QT_DATETIME_PROPERTY_GET(installDate,pamac_package_details_get_installdate(this))
 
-    PAMAC_QT_URL_PROPERTY_GET(screenshotUrl,pamac_package_details_get_screenshot(m_details.get()))
+    PAMAC_QT_DATETIME_PROPERTY_GET(buildDate,pamac_package_details_get_builddate(this))
 
-    PAMAC_QT_DATETIME_PROPERTY_GET(installDate,pamac_package_details_get_installdate(m_details.get()))
+    PAMAC_QT_STRING_PROPERTY_GET(installReason,pamac_package_details_get_reason(this))
 
-    PAMAC_QT_DATETIME_PROPERTY_GET(buildDate,pamac_package_details_get_builddate(m_details.get()))
+    PAMAC_QT_STRING_PROPERTY_GET(installedVersion,pamac_package_details_get_installed_version(this))
 
-    PAMAC_QT_STRING_PROPERTY_GET(installReason,pamac_package_details_get_reason(m_details.get()))
+    PAMAC_QT_STRINGLIST_PROPERTY_GET(depends,pamac_package_details_get_depends(this))
+    PAMAC_QT_STRINGLIST_PROPERTY_GET(optDepends,pamac_package_details_get_optdepends(this))
+    PAMAC_QT_STRINGLIST_PROPERTY_GET(licenses,pamac_package_details_get_licenses(this))
 
-    PAMAC_QT_STRING_PROPERTY_GET(installedVersion,pamac_package_details_get_installed_version(m_details.get()))
-
-    PAMAC_QT_STRINGLIST_PROPERTY_GET(depends,pamac_package_details_get_depends(m_details.get()))
-    PAMAC_QT_STRINGLIST_PROPERTY_GET(optDepends,pamac_package_details_get_optdepends(m_details.get()))
-    PAMAC_QT_STRINGLIST_PROPERTY_GET(licenses,pamac_package_details_get_licenses(m_details.get()))
-
-private:
-    std::shared_ptr<PamacPackageDetails> m_details;
 };
 } //namespace LibQPamac
 Q_DECLARE_METATYPE(LibQPamac::RepoPackage)
